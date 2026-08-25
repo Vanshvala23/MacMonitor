a67d5a630238b8994ce00134f368c8920e7227a9
VanshVala <102245923+Vanshvala23@users.noreply.github.com>
2026-08-25 23:04:29 +0530

    Initial Commit

---
+++ MacMonitor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MacMonitor
+//
+//  Created by Vansh Vala on 25/08/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MacMonitor/MacMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacMonitorApp.swift
+//  MacMonitor
+//
+//  Created by Vansh Vala on 25/08/26.
+//
+
+import SwiftUI
+
+@main
+struct MacMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
